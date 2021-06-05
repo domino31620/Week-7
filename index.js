07679de0be85b7df4cdac9adf3411e3d024f5a95
Dom <domnorris283@gmail.com>
2021-06-04 22:30:47 -0700

    Hash changes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const secret = "suPErdUperBigSeCRet";
 const PORT = 5000;
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 app.use(express.json());
 
 app.post('/create-token', (req,res) => {
@@ -40,5 +42,24 @@ app.get('/decode-token', (req,res)=>{
         }
     })
 })
+const tempPassword = 'ReskillAmericans123'
+let hashPassword;
+bcrypt.hash(tempPassword, saltRounds)
+ .then(hash => {
+    console.log(hash);
+    hashPassword= hash;
+    console.log(hashPassword);
+ });
+
+ app.post('/', (req,res) => {
+    const password = req.body.pass;
+    bcrypt.compare(password, hashPassword)
+        .then(result => {
+            console.log(result);
+            return res.status(200).json({ result})
+        }); 
+   
+}) 
+
 
 app.listen(PORT, ()=>{ console.log(`Server is listening on ${PORT}`)})
